f7bf544c723d0953772d86c267f8ed05f89eb6ed
mariamargaret <44648153+mariamargaret@users.noreply.github.com>
2024-09-25 16:12:56 +0530

    Add files via upload

---
+++ alb.tf
@@ -0,0 +1,80 @@
+# Create an Application Load Balancer
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets           = [aws_subnet.FW-MGMT-security[1].id, aws_subnet.FW-MGMT-security[2].id]
+
+  tags = {
+
+    Name = "app-alb"
+	}
+  
+}
+
+
+# Create a Target Group
+resource "aws_lb_target_group" "prodapp_tg" {
+  name        = "prodapp-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.Prod-vpc.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    protocol            = "HTTP"
+  }
+
+  tags = {
+    Name = "app-tg"
+  }
+}
+
+# Create a Listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prodapp_tg.arn
+  }
+}
+
+
+# Create a Security Group for the ALB
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.Prod-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# Register Targets (EC2 instances in this example)
+resource "aws_lb_target_group_attachment" "target" {
+  target_group_arn = aws_lb_target_group.prodapp_tg.arn
+  target_id        = aws_instance.prodapp_server.id  # Replace with your EC2 instance ID
+  port             = 80
+}
