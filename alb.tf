c7ff1c35a2f341dca7c4c489bd34b13f82f3597f
mariamargaret <44648153+mariamargaret@users.noreply.github.com>
2024-10-08 10:47:17 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -59,6 +59,12 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your IP or CIDR range
+}
 
   egress {
     from_port   = 0
